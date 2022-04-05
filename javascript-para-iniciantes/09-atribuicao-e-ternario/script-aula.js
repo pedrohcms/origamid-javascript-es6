4851405e4c9da543fa229a820186f7f6a023d6fb
pedrohcms <pedrohcms2018@gmail.com>
2022-04-05 19:30:11 -0300

    Adiciona aula de atribuição e ternário.

---
+++ javascript-para-iniciantes/09-atribuicao-e-ternario/script-aula.js
@@ -0,0 +1,19 @@
+var numero = 20;
+var numero2 = 10;
+
+numero += numero2; //numero = numero + 10;
+console.log(numero);
+console.log(numero2);
+
+var idade             = 20;
+var naoPossuiDiabetes = true;
+
+var podeBeber = (idade >= 18 && naoPossuiDiabetes) ? 'Pode beber' : 'Não pode beber';
+console.log(podeBeber);
+
+var possuiFaculdade = true;
+
+if(possuiFaculdade) 
+  console.log('Sim, possui');
+else
+  console.log('Não possui');
