3c11bae5d4c98d3f43a5a0fa04c09389a9e9de05
pedrohcms <pedrohcms2018@gmail.com>
2022-04-28 20:27:42 -0300

    Adiciona aula de scroll suave nos links internos.

---
+++ 02-dom-para-iniciantes/11-scroll-suave-link-interno/script.js
@@ -0,0 +1,64 @@
+function initTabNav() {
+  const tabMenu    = document.querySelectorAll('.js-tabmenu li');
+  const tabContent = document.querySelectorAll('.js-tabcontent section');
+
+  if(tabMenu.length && tabContent.length) {
+    tabContent[0].classList.add('ativo');
+
+    function activeTab(index) {
+      tabContent.forEach(section => section.classList.remove('ativo'));
+
+      tabContent[index].classList.add('ativo');
+    }
+
+    tabMenu.forEach((itemMenu, index) => {
+      itemMenu.addEventListener('click', () => activeTab(index));
+    });
+  }  
+}
+
+function initAccordion() {
+  const accordionList = document.querySelectorAll('.js-accordion dt');
+  const activeClas    = 'ativo';
+
+  if(accordionList.length) {
+    accordionList[0].classList.add(activeClas);
+    accordionList[0].nextElementSibling.classList.add(activeClas);
+
+    function activeAccordion() {
+      this.classList.toggle(activeClas);
+      this.nextElementSibling.classList.toggle(activeClas);
+    }
+
+    accordionList.forEach(item => item.addEventListener('click', activeAccordion))
+  }
+}
+
+function initScrollSuave() {
+  const linksInternos = document.querySelectorAll('.js-menu a[href^="#"]');
+
+  function scrollToSection(event) {
+    event.preventDefault();
+
+    const href    = event.currentTarget.getAttribute('href');
+    const section = document.querySelector(href);
+
+    section.scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    });
+
+    // Forma alternativa
+    // const topo    = section.offsetTop;
+    // window.scrollTo({
+    //   top: topo,
+    //   behavior: 'smooth'
+    // });
+  }
+
+  linksInternos.forEach(link => link.addEventListener('click', scrollToSection));  
+}
+
+initTabNav();
+initAccordion();
+initScrollSuave();
