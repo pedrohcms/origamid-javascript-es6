7b7db7fe8819ab1b216a0b3b8691806e361d55ee
pedrohcms <pedrohcms2018@gmail.com>
2022-05-10 20:02:01 -0300

    Adiciona aula de funções construtores.

---
+++ 04-objetos/01-constructor-functions/script-aula1.js
@@ -0,0 +1,18 @@
+function Carro(marcaAtribuida, precoAtribuido) {
+  this.marca = marcaAtribuida;
+  this.preco = precoAtribuido;
+}
+
+const honda = new Carro('Honda', 3000);
+const fiat = new Carro('Fiat', 4000);
+
+function Carro2(marca, precoInicial) {
+  this.taxa        = 1.2;
+  const precoFinal = precoInicial * this.taxa;
+
+  this.marca = marca;
+  this.preco = precoFinal;
+  console.log(this);
+}
+
+const mazda = new Carro2('Mazda', 5000);
